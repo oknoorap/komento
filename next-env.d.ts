150ab86d681674d46153e92223974e978082677d
Ribhararnus Pracutian <oknoorap@gmail.com>
2021-04-13 11:55:50 +0700

    fix: add `svg` typings

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,16 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
+declare module "*.svg" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
+
+declare module "*.svg?sprite" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
+
 declare module "*.doc.md" {
   const content: string;
   export default content;
