97f4569f9eb065f11b171bc017e8eb3e42886c8d
Ribhararnus Pracutian <oknoorap@gmail.com>
2021-04-14 14:09:41 +0700

    fix(layout): link logo to homepage

--- src/layouts/default/header.tsx
+++ src/layouts/default/header.tsx
@@ -1,12 +1,17 @@
-import { Flex, Box, Icon } from "@chakra-ui/react";
+import { Flex, Box, Icon, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 import { description } from "../../../next-seo.config";
 import Logo from "assets/svg/logo.svg?sprite";
 
 const HeaderLayout = () => {
   return (
-    <Flex flexDir="column" alignItems="center" mb="6">
-      <Icon as={Logo} w="60%" h="auto" />
+    <Flex flexDir="column" alignItems="center" mb="10">
+      <NextLink href="/" passHref>
+        <Link w="60%" _focus={{ outline: "none" }}>
+          <Icon as={Logo} w="100%" h="auto" />
+        </Link>
+      </NextLink>
       <Box color="gray.600" mt="-5">
         {description}
       </Box>
