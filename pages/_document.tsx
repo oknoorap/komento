793abc305bf1310f92008a714ff25ce446ae5089
Ribhararnus Pracutian <oknoorap@gmail.com>
2021-04-17 07:15:17 +0700

    feat: add favicons and web manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,38 @@ class MyDocument extends Document {
         <Head>
           <link rel="dns-prefetch" href="https://cdnjs.cloudflare.com" />
           <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color="#0fa7dc"
+          />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <meta name="apple-mobile-web-app-title" content="Komento" />
+          <meta name="application-name" content="Komento" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta
+            name="msapplication-config"
+            content="/favicons/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
